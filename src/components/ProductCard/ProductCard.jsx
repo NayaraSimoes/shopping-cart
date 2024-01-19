c78e6174b09f83a34b04c88699179067b618ea4f
Nayara Simões <93724124+NayaraSimoes@users.noreply.github.com>
2024-01-18 23:52:54 -0300

    em andamento
    em andamento

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import { IoCartOutline } from 'react-icons/io5';
+
+import './ProductCard.css';
+
+
+function ProductCard({ data }) {
+
+  const { title, thumbnail, price } = data;
+
+  return (
+    <section className="product-card">
+
+      <img 
+        src={thumbnail}
+        alt="product" 
+        className="card__image" />
+
+      <div className="card__infos">
+        <h2 className="card__price">{price}</h2>
+        <h2 className="card__title">{title}</h2>
+      </div>
+      <button type="button" className="button__add-cart">
+        <IoCartOutline />
+      </button>
+    </section>
+  );
+}
+
+//http://http2.mlstatic.com/D_932360-MLA46545868431_062021-W.jpg
+
+export default ProductCard;
+
+ProductCard.propTypes = {
+  data: propTypes.shape({}),
+}.isRequired;
