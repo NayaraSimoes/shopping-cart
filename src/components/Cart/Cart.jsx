d73b56b8e8a1a12ff4da62aeb11d097ff4e2762a
Nayara Simões <93724124+NayaraSimoes@users.noreply.github.com>
2024-01-31 17:28:12 -0300

    em andamento

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Cart.css';
+
+function Cart() {
+  return (
+    <section className="cart">
+      <div className="cart-itens">iten do carrinho</div>
+      <div className="cart-resume">resumo do carrinho</div>
+    </section>
+  );
+  //parei no aula 04 | 33:29
+}
+
+export default Cart;
